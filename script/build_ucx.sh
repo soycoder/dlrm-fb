a2c16c266859ab228e583d4740ca99812f033734
soycoder <om.apisith@gmail.com>
2021-10-03 16:00:37 +0000

    Fix CUDA Path 2

--- script/build_ucx.sh
+++ script/build_ucx.sh
@@ -4,7 +4,7 @@ set -o pipefail
 echo "INFO: Build UCX"
 cd "${SRC_DIR}/ucx"
 "${SRC_DIR}/ucx/autogen.sh"
-"${SRC_DIR}/ucx/contrib/configure-release-mt" --with-cuda="${CUDA_HOME}" --prefix="${UCX_INSTALL_DIR}" \
+"${SRC_DIR}/ucx/contrib/configure-release-mt" --prefix="${UCX_INSTALL_DIR}" \
     --build=x86_64-redhat-linux-gnu --host=x86_64-redhat-linux-gnu \
     --program-prefix= --disable-dependency-tracking \
     --exec-prefix=${UCX_INSTALL_DIR} --bindir=${UCX_INSTALL_DIR}/bin \
@@ -13,10 +13,10 @@ cd "${SRC_DIR}/ucx"
     --libexecdir=${UCX_INSTALL_DIR}/libexec --localstatedir=/var \
     --sharedstatedir=/var/lib --mandir=${UCX_INSTALL_DIR}/share/man \
     --infodir=${UCX_INSTALL_DIR}/share/info --disable-optimizations \
-    --enable-cma --with-cuda  \
+    --enable-cma   \
     --with-verbs --with-cm --with-knem --with-rdmacm \
     --without-rocm --without-xpmem --without-ugni \
-    --without-java  
+    --without-java  --with-cuda=/usr/local/cuda
 make -j install
 echo "${UCX_INSTALL_DIR}/lib" > /etc/ld.so.conf.d/ucx.conf
 ldconfig
