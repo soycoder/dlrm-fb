e4a50d5b1e38e101964eb24b047b25a349dd97f2
soycoder <43911669+soycoder@users.noreply.github.com>
2021-10-04 16:10:53 +0700

    Add Path cublas_v2.h

--- script/install_torch_ucc.sh
+++ script/install_torch_ucc.sh
@@ -6,6 +6,7 @@ echo "INFO: Install Torch-UCC (UCC version)"
 export UCX_HOME=${UCX_INSTALL_DIR}
 export UCC_HOME=${UCC_INSTALL_DIR}
 export WITH_CUDA=${CUDA_HOME}
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/cuda-10.2/targets/x86_64-linux/include/
 cd "${TORCH_UCC_INSTALL_DIR}"
 python setup.py install bdist_wheel
 pip list | grep torch
