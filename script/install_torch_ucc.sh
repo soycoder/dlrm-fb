3e3eaabf2924cba709c47dcd09c83def9f041305
soycoder <43911669+soycoder@users.noreply.github.com>
2021-10-05 23:02:02 +0700

    Revert "Remove bdist_wheel"
    This reverts commit 5a1b849e335d5334688b4eeca499215546ac2401.

--- script/install_torch_ucc.sh
+++ script/install_torch_ucc.sh
@@ -8,6 +8,6 @@ export UCC_HOME=${UCC_INSTALL_DIR}
 export WITH_CUDA=${CUDA_HOME}
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/usr/local/cuda-10.2/targets/x86_64-linux/include/
 cd "${TORCH_UCC_INSTALL_DIR}"
-python setup.py install
+python setup.py install bdist_wheel
 pip list | grep torch
 python -c 'import torch, torch_ucc'
