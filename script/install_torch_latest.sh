377a1aaded2bde07bbe005a0fb3e2cc4c8c04b06
soycoder <43911669+soycoder@users.noreply.github.com>
2021-10-06 00:16:25 +0700

    Create install_torch_latest.sh

---
+++ script/install_torch_latest.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -eEx
+set -o pipefail
+
+pip install --prefix=/usr/local --no-cache-dir --default-timeout=900 numpy
+pip install --prefix=/usr/local --no-cache-dir --default-timeout=900 --pre torch -f https://download.pytorch.org/whl/nightly/cu113/torch_nightly.html
+pip install --prefix=/usr/local "git+https://github.com/mlperf/logging.git@0.7.1"
