7dd97ea7a83d481f35ef4305b13d452aba20e49f
soycoder <43911669+soycoder@users.noreply.github.com>
2021-10-14 11:45:57 +0700

    Create build_ompi.sh

---
+++ script/build_ompi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eEx
+set -o pipefail
+
+echo "INFO: Build OMPI"
+OMPI_SRC_DIR="${SRC_DIR}/ompi"
+cd "${OMPI_SRC_DIR}"
+"${OMPI_SRC_DIR}/autogen.pl"
+"${OMPI_SRC_DIR}/configure" --prefix=/opt/ompi --with-ucx=/opt/ucx --with-ucc=/opt/ucc \
+make -j install
+echo "${OMPI_INSTALL_DIR}/lib" > /etc/ld.so.conf.d/ompi.conf
+ldconfig
+# ldconfig -p | grep -i libucc
