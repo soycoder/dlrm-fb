055b3c64c6963ec7066ecbfd0a807cbf3319934a
soycoder <43911669+soycoder@users.noreply.github.com>
2021-10-14 11:44:24 +0700

    Update build_ucc.sh

--- script/build_ucc.sh
+++ script/build_ucc.sh
@@ -7,7 +7,7 @@ cd "${UCC_SRC_DIR}"
 "${UCC_SRC_DIR}/autogen.sh"
 mkdir -p "${UCC_SRC_DIR}/build"
 cd "${UCC_SRC_DIR}/build"
-"${UCC_SRC_DIR}/configure" --prefix=/opt/ucc --with-ucx=/opt/ucx --without-nccl \
+"${UCC_SRC_DIR}/configure" --prefix=/opt/ucc --with-ucx=/opt/ucx \
     --with-cuda=/usr/local/cuda
 make -j install
 echo "${UCC_INSTALL_DIR}/lib" > /etc/ld.so.conf.d/ucc.conf
