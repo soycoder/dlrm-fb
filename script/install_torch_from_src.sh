9ba7855239ac68e37d06bb75f7ed151f6e080432
soycoder <om.apisith@gmail.com>
2021-10-15 02:27:52 +0000

    Add Pytorch From src

---
+++ script/install_torch_from_src.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eEx
+set -o pipefail
+
+cd /tmp 
+git clone https://github.com/pytorch/pytorch.git
+cd /tmp/pytorch
+git submodule sync --recursive
+git submodule update --init --recursive
+git checkout v1.9.1
+pip install -r requirements.txt
+export TORCH_CUDA_ARCH_LIST="7.0 8.0+PTX"
+export USE_GLOO=1
+export USE_DISTRIBUTED=1
+export USE_OPENCV=0
+export USE_CUDA=1
+export USE_NCCL=1
+export USE_MKLDNN=0
+export BUILD_TEST=0
+export USE_FBGEMM=0
+export USE_NNPACK=0
+export USE_QNNPACK=0
+export USE_XNNPACK=0
+export USE_KINETO=1
+export MAX_JOBS=$(($(nproc)-1))
+python setup.py install
+
+rm -rf /tmp/pytorch
